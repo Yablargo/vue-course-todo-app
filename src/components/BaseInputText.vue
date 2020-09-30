3254aa9ef2fdc41b23a376dc8f90cef9c2b76612
lsshawn <coffee.or.tea88@gmail.com>
2020-09-30 12:05:26 +0800

    start

---
+++ src/components/BaseInputText.vue
@@ -0,0 +1,34 @@
+<template>
+  <input type="text" class="input" :value="value" v-on="listeners" />
+</template>
+
+<script>
+export default {
+  // TODO: replace with a data
+  props: {
+    value: {
+      type: String,
+      default: "",
+    },
+  },
+  computed: {
+    // TODO: delete this and add a method that calls Vuex action 'addTodo'
+    listeners() {
+      return {
+        // Pass all component listeners directly to input
+        ...this.$listeners,
+        // Override input listener to work with v-model
+        input: (event) => this.$emit("input", event.target.value),
+      };
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.input {
+  width: 100%;
+  padding: 8px 10px;
+  border: 1px solid #32485f;
+}
+</style>
---
+++ src/components/TodoListItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <li>
+    {{ todo.text }}
+    <!-- TODO: Use Vuex action that calls a mutation -->
+    <button @click="$emit('remove', todo.id)">
+      X
+    </button>
+  </li>
+</template>
+
+<script>
+export default {
+  // TODO: Use Vuex state
+  props: {
+    todo: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
