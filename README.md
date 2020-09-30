4ad9934e4ab2566a85a9207ac9b3e90fcb12079f
Matthew Galligan <matthew.galligan@navy.mil>
2020-09-30 10:34:18 -0400

    Changes

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # vue-course-todo-app
+
+1. Install Vuex and setup the store folder.
+
+- Update main.js
+- Create:
+  - store/store.js
+  - store/modules/todo.js
+
+2. Create state first. Look at the 3 components and see what data can be moved to state.
+
+- todos
+
+3. Move the 'addTodo' and 'removeTodo' functions into Vuex action. Create a mutation to set the store state.
+
+4. Create mutations.
---
+++ src/store/modules/todo.js
@@ -0,0 +1,30 @@
+const state = {
+  todos: [
+    {
+      id: 1,
+      text: "Learn Vue",
+    },
+    {
+      id: 2,
+      text: "Learn about single-file components",
+    },
+    {
+      id: 3,
+      text: "Fall in love",
+    },
+  ],
+};
+
+const getters = {};
+
+const mutations = {};
+
+const actions = {};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  mutations,
+  actions,
+};
